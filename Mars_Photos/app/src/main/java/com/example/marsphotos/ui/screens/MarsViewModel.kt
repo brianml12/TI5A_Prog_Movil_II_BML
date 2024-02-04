82ce0ac39a8dabbd5d548dd8f1daafd6f0ede7c6
BrianML <s20120168@alumnos.itsur.edu.mx>
2024-02-03 20:23:10 -0600

    Crea la capa de datos

--- Mars_Photos/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ Mars_Photos/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
 import com.example.marsphotos.network.MarsApi
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
@@ -52,7 +53,8 @@ class MarsViewModel : ViewModel() {
             viewModelScope.launch {
                 marsUiState = MarsUiState.Loading
                 marsUiState = try {
-                    val listResult = MarsApi.retrofitService.getPhotos()
+                    val marsPhotosRepository = NetworkMarsPhotosRepository()
+                    val listResult = marsPhotosRepository.getMarsPhotos()
                     MarsUiState.Success(
                         "Success: ${listResult.size} Mars photos retrieved"
                     )
