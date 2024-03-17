eb2b320e5b72e443773a4867899ae50346feb34d
BrianML <s20120168@alumnos.itsur.edu.mx>
2024-03-16 19:41:19 -0600

    Se agrego un boton y archivo de preferencias

--- AplicacionLlamada/app/src/main/java/com/brian_angel/respuestallamada/ui/screens/HomeScreen.kt
+++ AplicacionLlamada/app/src/main/java/com/brian_angel/respuestallamada/ui/screens/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -87,6 +88,20 @@ fun contentScaffold(homeViewModel: HomeViewModel){
             },
             label = { Text("Ingresa el mensaje") }
         )
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    dimensionResource(R.dimen.padding_start_textField),
+                    end = dimensionResource(R.dimen.padding_end_textField)
+                ),
+            onClick = {
+
+            }
+        ) {
+            Text("Guardar")
+        }
     }
 }
 
---
+++ AplicacionLlamada/app/src/main/java/com/brian_angel/respuestallamada/utils/PreferenceUtils.kt
@@ -0,0 +1,17 @@
+package com.brian_angel.respuestallamada.utils
+
+import android.content.Context
+
+class PreferenceUtils {
+    fun saveString(ctx: Context, key: String, value: String) {
+        val sharedPref = ctx.getSharedPreferences("llamadasApp", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    fun getString(ctx: Context, key: String, defaultValue: String): String? {
+        val sharedPref = ctx.getSharedPreferences("llamadasApp", Context.MODE_PRIVATE)
+        return sharedPref.getString(key, defaultValue)
+    }
+}
